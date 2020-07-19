9b2ef77fb0ac80bfb333bbb289764dd76bae0310
markandmark1111@gmail.com <markandmark1111@gmail.com>
2020-07-19 11:05:03 +0200

    Add project files.

---
+++ TenSecondsAndProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TenSecondsAndProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program is treminated after 10 seconds");
+            Console.WriteLine(" ");
+
+            var myStartTime = DateTime.Now;
+            var temp = myStartTime;
+            DateTime myCurrentTime;
+            try
+            {
+                for (long i = 0; i < long.MaxValue; i++)
+                {
+                    // Console.WriteLine(i);
+                    myCurrentTime = DateTime.Now;
+                    if (Math.Abs((myCurrentTime - myStartTime).TotalSeconds) >= 10)
+                        throw new TimeoutException();
+                }
+            }
+            catch (TimeoutException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("--- Your time is expired ---");
+                Console.WriteLine(" ");
+            }
+            finally
+            {
+                Console.WriteLine("Hello again!!!!!!!!");
+                Console.WriteLine($"Start time: {temp.TimeOfDay}");
+                Console.WriteLine($"Start time: {DateTime.Now.TimeOfDay}");
+            }
+        }
+    }
+}
